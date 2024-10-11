b84600e753b532710a79719bfc5fd64c4a044cc1
ARGAUD <romain943602@gmail.com>
2024-10-10 23:05:29 -0400

    Added fonction to communicate with the MC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.github.mik3y:usb-serial-for-android:3.8.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
