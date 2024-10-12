fc30f9753249419f4009ddf2abd8775ff0f98e28
ARGAUD <romain943602@gmail.com>
2024-10-12 19:13:09 -0400

    Cleaned and a add an input to test the connection

--- app/src/main/java/com/example/klavier/MainActivity.kt
+++ app/src/main/java/com/example/klavier/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.klavier
 
-import android.os.Build
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.hardware.usb.UsbDevice
-import android.hardware.usb.UsbDeviceConnection
 import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.util.Log
@@ -18,100 +14,46 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.klavier.ui.theme.KlavierTheme
-import com.hoho.android.usbserial.driver.UsbSerialDriver
-import com.hoho.android.usbserial.driver.UsbSerialPort
-import com.hoho.android.usbserial.driver.UsbSerialProber
 
 
 class MainActivity : ComponentActivity() {
 
-    var m_manager : UsbManager? = null
-    var m_device : UsbDevice? = null
-    var m_driver : UsbSerialDriver? = null
-    var m_connection : UsbDeviceConnection? = null
-    var m_port : UsbSerialPort? = null
+    var usbController : USBController? = null
     val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
-    val WRITE_WAIT_MILLIS = 2000;
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        usbController = USBController(this)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             KlavierTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    keyboardTest(
+                        usbController = usbController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
 
-        m_manager = getSystemService(Context.USB_SERVICE) as UsbManager
+
         val filter = IntentFilter()
         filter.addAction(ACTION_USB_PERMISSION)
         filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
         filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED)
         registerReceiver(broadcastReceiver, filter)
 
-        USBConnect()
+        usbController?.ManageUSB()
 
-    }
-
-    fun USBConnect() {
-        val availableDrivers : List<UsbSerialDriver> = UsbSerialProber.getDefaultProber().findAllDrivers(m_manager)
-        if (availableDrivers.isEmpty()) {
-            return
-        }
-        else {
-            m_driver = availableDrivers[0]
-            if (m_driver != null) {
-                m_device = m_driver!!.device
-                Log.i("USB", "USB device name: " + m_device!!.deviceName)
-
-                val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_MUTABLE else 0
-                val intent: PendingIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), flags)
-
-
-                if (m_manager!!.hasPermission(m_driver!!.device)) {
-                    // Permission already granted, no need to request
-                    connect()  // Skip to opening the connection
-                    Log.i("USB", "USB permission not requested")
-                } else {
-                    // Request permission
-                    m_manager!!.requestPermission(m_driver!!.device, intent)
-                    Log.i("USB", "USB permission requested")
-                }
-
-            }
-        }
-
-
-    }
-
-    fun USBWrite(data: String) {
-        Log.i("USB", "USB write")
-        m_port?.write((data+"\r\n").toByteArray(), WRITE_WAIT_MILLIS)
-    }
-
-    fun USBRead() {
-        //TODO
-    }
-
-    fun connect(){
-        m_connection = m_manager?.openDevice(m_driver?.device)
-        m_port = m_driver?.ports?.get(0)
-        if (m_port != null) {
-            m_port!!.open(m_connection)
-            m_port!!.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
-            USBWrite("red")
-        } else {
-            Log.i("USB", "port is null")
-        }
     }
 
     val broadcastReceiver = object : BroadcastReceiver() {
@@ -120,23 +62,20 @@ class MainActivity : ComponentActivity() {
                 val granted = p1.extras?.getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED) ?: false
                 Log.i("USB", p1.extras.toString())
                 if (granted) {
-                    connect()
+                    usbController?.connectToDevice()
                 } else {
                     Log.i("USB", "Permission denied for device")
                 }
             } else if (p1.action == UsbManager.ACTION_USB_DEVICE_ATTACHED) {
-                USBConnect()
+                usbController?.ManageUSB()
             } else if (p1.action == UsbManager.ACTION_USB_DEVICE_DETACHED) {
-                m_port?.close()
-                m_connection?.close()
+                usbController?.disconnectFromDevice()
             }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
-        // Unregister the BroadcastReceiver to prevent memory leaks
         unregisterReceiver(broadcastReceiver)
     }
 }
@@ -145,10 +84,38 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun keyboardTest(usbController: USBController? = null, modifier: Modifier = Modifier) {
+    var input by remember { mutableStateOf("") }
+    val backspace : String = "\b"
+    TextField(
+        value = input,
+        onValueChange = { newText ->
+
+            val addedText =if (newText.isEmpty()) {
+                ""
+            }
+            else if ((newText.length > input.length)) {
+                newText.substring(input.length)
+            } else if ((newText.length < input.length) and (newText.last() != '.')) {
+                backspace.repeat(input.length - newText.length)
+            }
+            else if(newText.last() == '.') {
+                newText.substring(input.length-1)
+            }
+            else{
+                ""
+            }
+            input = if (addedText != ".") newText else ""
+
+            if (addedText.isNotEmpty()) {
+                usbController?.USBWrite(addedText)
+                Log.i("USB", addedText)
+            }
+                                        },
+        label = { Text("Label") }
+
     )
 }
+
+
 
